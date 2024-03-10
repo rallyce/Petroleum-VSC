70a239c43cb0a15e4669d25968015c9dd05256b0
Omar Yesid Zuniga Calderon <ozunigacalderon@gmail.com>
2024-03-09 23:44:38 -0500

    transparency effect backup in main.css

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,7 @@
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+        <div class="card bg-dark text-white"  style="border-radius: 1rem;">
           <div class="card-body p-5 text-center">
             <img src="@/assets/images/logo.png" style="margin: auto; width: 170px;"/>
             <div class="mb-md-5 mt-md-4 pb-5">
